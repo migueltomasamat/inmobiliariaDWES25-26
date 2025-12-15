8af9f02fbf8fd92e0bc096b4782a2b5d096f2efb
matomas <migueltomas.informatica@iespacomolla.es>
2025-12-15 20:21:47 +0100

    Migraciones, relaciones y Seeders

---
+++ app/Http/Controllers/PerfilController.php
@@ -0,0 +1,66 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Http\Requests\StorePerfilRequest;
+use App\Http\Requests\UpdatePerfilRequest;
+use App\Models\Perfil;
+
+class PerfilController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     */
+    public function store(StorePerfilRequest $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     */
+    public function show(Perfil $perfil)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     */
+    public function edit(Perfil $perfil)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     */
+    public function update(UpdatePerfilRequest $request, Perfil $perfil)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     */
+    public function destroy(Perfil $perfil)
+    {
+        //
+    }
+}
